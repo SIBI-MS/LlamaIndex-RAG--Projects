ecf074e75fa220c113b53d9bff12d289df01b641
sibims <sibima00@gmail.com>
2024-05-03 11:57:19 +0530

    stored the data

--- bot.py
+++ bot.py
@@ -57,23 +57,66 @@ def extract_terms(documents, term_extract_str, llm_name, model_temperature, api_
 
 
 
+if "all_terms" not in st.session_state:
+    st.session_state["all_terms"] = DEFAULT_TERM_STR
+...
 
+
+def insert_terms(terms_to_definition):
+    for term, definition in terms_to_definition.items():
+        doc = Document(text=f"Term: {term}\nDefinition: {definition}")
+        st.session_state["llama_index"].insert(doc)
+
+
+@st.cache_resource
+def initialize_index(llm_name, model_temperature, api_key):
+    """Create the VectorStoreIndex object."""
+    Settings.llm = get_llm(llm_name, model_temperature, api_key)
+
+    index = VectorStoreIndex([])
+
+    return index, llm
 
 
 
 with upload_tab:
     st.subheader("Extract and Query Definitions")
-    document_text = st.text_area("Or enter raw text")
-    if st.button("Extract Terms and Definitions") and document_text:
-        with st.spinner("Extracting..."):
-            extracted_terms = extract_terms(
-                [Document(text=document_text)],
-                term_extract_str,
-                llm_name,
-                model_temperature,
-                api_key,
-            )
-        st.write(extracted_terms)
+    if st.button("Initialize Index and Reset Terms"):
+        st.session_state["llama_index"] = initialize_index(
+            llm_name, model_temperature, api_key
+        )
+        st.session_state["all_terms"] = {}
+
+    if "llama_index" in st.session_state:
+        st.markdown(
+            "Either upload an image/screenshot of a document, or enter the text manually."
+        )
+        document_text = st.text_area("Or enter raw text")
+        if st.button("Extract Terms and Definitions") and (uploaded_file or document_text):
+            st.session_state["terms"] = {}
+            terms_docs = {}
+            with st.spinner("Extracting..."):
+                terms_docs.update(
+                    extract_terms(
+                        [Document(text=document_text)],
+                        term_extract_str,
+                        llm_name,
+                        model_temperature,
+                        api_key,
+                    )
+                )
+            st.session_state["terms"].update(terms_docs)
+
+        if "terms" in st.session_state and st.session_state["terms"]:
+            st.markdown("Extracted terms")
+            st.json(st.session_state["terms"])
+
+            if st.button("Insert terms?"):
+                with st.spinner("Inserting terms"):
+                    insert_terms(st.session_state["terms"])
+                st.session_state["all_terms"].update(st.session_state["terms"])
+                st.session_state["terms"] = {}
+                st.experimental_rerun()
         
         
         
